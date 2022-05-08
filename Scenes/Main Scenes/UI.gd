0c12b7dd8eb65b5385ead86bf56fdb09f31968f8
Vinz Myko Del Rosario <vinzmykodelrosario@gmail.com>
2022-05-08 21:01:26 +0100

    5.2 feat: variable tower range indicators
    When you click on a turret it now spawns a indicator showing it's range, and this range indicator will now change depending on it's tower type, this is set up on the GameData.gd singleton.

--- Scenes/Main Scenes/UI.gd
+++ Scenes/Main Scenes/UI.gd
@@ -6,8 +6,17 @@ func set_tower_preview(tower_type, mouse_position):
 	drag_tower.set_name("DragTower") #Sets name in scene tree
 	drag_tower.modulate = Color("ad54ff3c")
 	
+	var range_texture = Sprite.new()
+	range_texture.position = Vector2(32, 32)
+	var scaling : float = GameData.tower_data[tower_type]["range"] / 600.0
+	range_texture.scale = Vector2(scaling, scaling)
+	var texture = load("res://Assets/Tilesets/range_overlay.png")
+	range_texture.texture = texture
+	range_texture.modulate = Color("ad54ff3c")
+	
 	var control = Control.new()
 	control.add_child(drag_tower, true)
+	control.add_child(range_texture, true)
 	control.rect_position = mouse_position
 	control.set_name("TowerPreview")
 	add_child(control, true)
@@ -22,5 +31,6 @@ func update_tower_preview(new_position, color):
 	if get_node("TowerPreview/DragTower").modulate != Color(color): # If colour is not the same as were
 		# getting received then change it to that colour
 		get_node("TowerPreview/DragTower").modulate = Color(color)
+		get_node("TowerPreview/Sprite").modulate = Color(color)
 		
 	
---
+++ Singletons/GameData.gd
@@ -0,0 +1,13 @@
+extends Node
+
+var tower_data : Dictionary = {
+	"GunT1": {
+		"damage" : 20,
+		"rof" : 1,
+		"range": 350},
+	"MissileT1":{
+		"damage": 100,
+		"rof": 3,
+		"range": 550}
+	}
+	
